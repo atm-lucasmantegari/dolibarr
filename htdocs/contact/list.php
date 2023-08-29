20624811276986f5ae62d5efc5e864355c48387e
ATM-Lucas <lucas.mantegari@atm-consulting.fr>
2023-08-29 15:24:36 +0200

    Paremètres de la fonction getNomUrl mauvaise

--- htdocs/contact/list.php
+++ htdocs/contact/list.php
@@ -1348,7 +1348,7 @@ while ($i < min($num, $limit)) {
 		if ($obj->socid) {
 			$objsoc = new Societe($db);
 			$objsoc->fetch($obj->socid);
-			print $objsoc->getNomUrl(1, 'customer', 100, 0, 1, empty($arrayfields['s.name_alias']['checked']) ? 0 : 1);
+			print $objsoc->getNomUrl(1, '', 100, 0, 1, empty($arrayfields['s.name_alias']['checked']) ? 0 : 1);
 		} else {
 			print '&nbsp;';
 		}
